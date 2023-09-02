6e30c5298d7a5bc5ae6d7416e3de6786fdba002c
Lise <35637366+luararamos@users.noreply.github.com>
2023-09-02 13:15:52 -0300

    MainActivity and ProfileFragment

---
+++ app/src/main/java/com/example/myinstagram/profile/view/ProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.example.myinstagram.profile.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myinstagram.R
+
+class ProfileFragment: Fragment() {
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
