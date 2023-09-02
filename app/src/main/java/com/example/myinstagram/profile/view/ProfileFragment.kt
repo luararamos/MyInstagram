79a9244f20e59fd6b643ddbff33007c8ab83094d
Lise <35637366+luararamos@users.noreply.github.com>
2023-09-02 14:32:47 -0300

    scrollFlags: scroll dinamico

--- app/src/main/java/com/example/myinstagram/profile/view/ProfileFragment.kt
+++ app/src/main/java/com/example/myinstagram/profile/view/ProfileFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myinstagram.R
 
 class ProfileFragment: Fragment() {
@@ -14,5 +17,31 @@ class ProfileFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val rv = view.findViewById<RecyclerView>(R.id.profile_rv)
+        rv.layoutManager  = GridLayoutManager(requireContext(), 3)
+        rv.adapter = PostAdapter()
+
+
+    }
+    private class PostAdapter: RecyclerView.Adapter<PostAdapter.PostViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            val inflater =LayoutInflater.from(parent.context).inflate(R.layout.item_profile_grid, parent, false)
+            return PostViewHolder(inflater)
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        private class PostViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+            fun bind(image: Int){
+                itemView.findViewById<ImageView>(R.id.item_profile_img_grid).setImageResource(image)
+            }
+        }
     }
 }
