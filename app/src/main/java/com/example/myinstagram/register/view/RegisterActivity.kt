1ad1a11e09484ac42262c5214c47817ad280d2b4
Lise <35637366+luararamos@users.noreply.github.com>
2023-10-23 19:52:25 -0300

    Abrir camera

--- app/src/main/java/com/example/myinstagram/register/view/RegisterActivity.kt
+++ app/src/main/java/com/example/myinstagram/register/view/RegisterActivity.kt
@@ -3,19 +3,32 @@ package com.example.myinstagram.register.view
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.example.myinstagram.R
+import com.example.myinstagram.databinding.ActivityRegisterBinding
 import com.example.myinstagram.main.view.MainActivity
 import com.example.myinstagram.register.view.CropperImageFragment.Companion.KEY_URI
 import com.example.myinstagram.register.view.RegisterNamePasswordFragment.Companion.KEY_EMAIL
 import com.example.myinstagram.register.view.WelcomeFragment.Companion.KEY_NAME
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class RegisterActivity : AppCompatActivity(), FragmentAttachListener {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var currentPhoto: Uri
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val fragment = RegisterEmailFragment()
 
@@ -56,17 +69,48 @@ class RegisterActivity : AppCompatActivity(), FragmentAttachListener {
         startActivity(intent)
     }
 
-    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {  uri: Uri? ->
-       val fragment = CropperImageFragment().apply {
-           arguments = Bundle().apply {
-               putParcelable(KEY_URI, uri)
-           }
-       }
-        replaceFragment(fragment)
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uri?.let {
+            openImageCropper(it)
+        }
     }
+    private val getCamera = registerForActivityResult(ActivityResultContracts.TakePicture()) { saved ->
+        if (saved){
+            openImageCropper(currentPhoto)
+        }
+    }
+
     override fun goToGalleryScreen() {
         getContent.launch("image/*")
     }
+
+    override fun goToCameraScreen() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            val photoFile: File? = try {
+                createImageFile()
+
+            } catch (e: IOException) {
+                Log.e("RegisterActivity", e.message, e)
+                null
+            }
+            photoFile?.also {
+                val photoUri =
+                    FileProvider.getUriForFile(this, "com.example.myinstagram.fileprovider", it)
+                currentPhoto = photoUri
+                getCamera.launch(photoUri)
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timestamp}", ".jpg", dir)
+    }
+
+
 
     private fun replaceFragment(fragment: Fragment) {
         if (supportFragmentManager.findFragmentById(R.id.register_fragment) == null) {
@@ -80,5 +124,14 @@ class RegisterActivity : AppCompatActivity(), FragmentAttachListener {
             addToBackStack(null)
             commit()
         }
+    }
+
+    private fun openImageCropper(uri: Uri){
+        val fragment = CropperImageFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(KEY_URI, uri)
+            }
+        }
+        replaceFragment(fragment)
     }
 }
